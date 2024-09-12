f121a14fed43bbdce68c6bafddc5b7ee87150647
Allie.Tennyson <agtennyson@gmail.com>
2024-09-11 20:08:03 -0700

    Add dynamic values to templates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,28 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {HousingLocationComponent} from '../housing-location/housing-location.component';
-import { HousingLocation } from '../housinglocation';
+import {HousingLocation} from '../housinglocation';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, HousingLocationComponent],
   template: `
-  <section>
-    <form>
-      <input type="text" placeholder="Filter by city" />
-      <button class="primary" type="button">Search</button>
-    </form>
-  </section>
-  <section class="results">
-    <app-housing-location [housingLocation]="housingLocation"]></app-housing-location>
-  </section>
-`,
-  styles: ``
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+    </section>
+  `,
+  styleUrls: ['./home.component.css'],
 })
-
 export class HomeComponent {
   readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
-  
+
   housingLocation: HousingLocation = {
     id: 9999,
     name: 'Test Home',
